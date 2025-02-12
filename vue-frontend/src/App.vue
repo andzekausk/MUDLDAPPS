337e6c6de9173fa9c7a698adefd36f5907360867
andzekausk <s23kausandz@venta.lv>
2025-02-12 13:19:15 +0200

    Redirect to login on logout

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -7,7 +7,7 @@
       <router-link v-if="authStore.currentRole=='pārvaldnieks'" to="/">Rezervāciju veidošana </router-link> |
       <router-link v-if="authStore.currentRole!='administrators'" to="/">Pieteikt problēmu </router-link> |
       <router-link v-if="authStore.currentRole=='administrators'" to="/admin">Lietotāju pārvaldība </router-link> | 
-      <button v-if="authStore.user" @click="authStore.logout">Logout</button>
+      <button v-if="authStore.user" @click="logoutAndRedirect">Logout</button>
       <select v-if="authStore.roles.length > 1" v-model="authStore.currentRole">
       <option v-for="role in authStore.roles" :key="role" :value="role">{{ role }}</option>
       </select>
@@ -21,11 +21,19 @@
 
 <script>
 import { useAuthStore } from "./store/auth";
+import { useRouter } from "vue-router"; 
 
 export default {
   setup() {
     const authStore = useAuthStore();
-    return { authStore };
+    const router = useRouter();
+
+    const logoutAndRedirect = () => {
+      authStore.logout();
+      router.push("/login"); 
+    };
+
+  return { authStore, logoutAndRedirect };
   }
 };
 </script>
