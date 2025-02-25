5f01269c7453d8ee0e39b05739c65aa9998403cc
andzekausk <s23kausandz@venta.lv>
2025-02-25 15:02:34 +0200

    Replaced vuetify vcalendar with FullCalendar
    For better customisation. Created a calendar view. Removed vuetify related dependencies.

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -3,12 +3,15 @@ import './style.css'
 import App from './App.vue'
 import router from "./router";
 import { createPinia } from "pinia";
-import vuetify from "./plugins/vuetify";
-// import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css';
+
+import FullCalendar from '@fullcalendar/vue3';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+// import interactionPlugin from '@fullcalendar/interaction';
 
 const app = createApp(App);
+
+app.component('FullCalendar', FullCalendar);
+
 app.use(createPinia());
 app.use(router);
-app.use(vuetify);
 app.mount("#app");
--- vue-frontend/src/views/ReservationOverview.vue
+++ vue-frontend/src/views/ReservationOverview.vue
@@ -1,96 +1,92 @@
 <template>
-    <div>
-        <v-sheet
-            class="d-flex"
-            height="54"
-            tile
-        >
-            <v-select
-                v-model="type"
-                :items="types"
-                class="ma-2"
-                density="compact"
-                label="View Mode"
-                variant="outlined"
-                hide-details
-            ></v-select>
-            <v-select
-                v-model="weekday"
-                :items="weekdays"
-                class="ma-2"
-                density="compact"
-                label="weekdays"
-                variant="outlined"
-                hide-details
-            ></v-select>
-        </v-sheet>
-        <v-sheet>
-            <v-calendar
-                ref="calendar"
-                v-model="value"
-                :events="events"
-                :view-mode="type"
-                :weekdays="weekday"
-            ></v-calendar>
-        </v-sheet>
-    </div>
+  <div class='demo-app'>
+    <FullCalendar
+      class='demo-app-calendar'
+      ref="fullCalendar"
+      :options='calendarOptions'
+    >
+      <template v-slot:eventContent='arg'>
+        <b>{{ arg.timeText }}</b>
+        <i>{{ arg.event.title }}</i>
+      </template>
+    </FullCalendar>
+  </div>
 </template>
-  <script>
-    import { useDate } from 'vuetify'
-  
-    export default {
-      data: () => ({
-        type: 'week',
-        types: ['month', 'week', 'day'],
-        weekday: [0, 1, 2, 3, 4, 5, 6],
-        weekdays: [
-          { title: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-          { title: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-          { title: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-          { title: 'Mon, Wed, Fri', value: [1, 3, 5] },
+
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from "@fullcalendar/list";
+
+export default {
+
+  components: {
+    FullCalendar // makes the <FullCalendar> tag available
+  },
+
+  data: function() {
+    return {
+      calendarOptions: {
+        plugins: [
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin, // needed for dateClick
+          listPlugin
         ],
-        value: [new Date()],
-        events: [],
-        colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-        titles: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
-      }),
-      mounted () {
-        const adapter = useDate()
-        this.getEvents({ start: adapter.startOfDay(adapter.startOfMonth(new Date())), end: adapter.endOfDay(adapter.endOfMonth(new Date())) })
-      },
-      methods: {
-        getEvents ({ start, end }) {
-          const events = []
-  
-          const min = start
-          const max = end
-          const days = (max.getTime() - min.getTime()) / 86400000
-          const eventCount = this.rnd(days, days + 20)
-  
-          for (let i = 0; i < eventCount; i++) {
-            const allDay = this.rnd(0, 3) === 0
-            const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-            const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-            const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-            const second = new Date(first.getTime() + secondTimestamp)
-  
-            events.push({
-              title: this.titles[this.rnd(0, this.titles.length - 1)],
-              start: first,
-              end: second,
-              color: this.colors[this.rnd(0, this.colors.length - 1)],
-              allDay: !allDay,
-            })
-          }
-  
-          this.events = events
+        headerToolbar: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
         },
-        getEventColor (event) {
-          return event.color
+        initialView: 'timeGridWeek',
+        firstDay: 1, 
+        locale: 'lv',
+        editable: true,
+        events: [
+          { title: 'Event Now', start: new Date() }
+        ],
+        dateClick: this.handleDateClick,
+        buttonText: {
+          today: 'Šodien',
+          month: 'Mēnesis',
+          week: 'Nedēļa',
+          day: 'Diena',
+          list: 'Saraksts'
         },
-        rnd (a, b) {
-          return Math.floor((b - a + 1) * Math.random()) + a
-        },
-      },
+        allDayText: 'Visu dienu'
+      }
     }
-  </script>
+  },
+
+  methods: {
+
+    handleDateClick(arg) {
+      if (confirm('Izveidot jaunu rezervāciju ' + arg.dateStr + ' ?')) {
+        let calendarApi = this.$refs.fullCalendar.getApi()
+        calendarApi.addEvent({
+          title: 'Rezervācija',
+          start: arg.date,
+          allDay: arg.allDay
+        })
+      }
+    }
+  }
+}
+
+</script>
+
+<style lang='css'>
+
+.demo-app {
+  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+  font-size: 14px;
+}
+
+.demo-app-calendar {
+  margin: 0 auto;
+  max-width: 900px;
+}
+
+</style>
